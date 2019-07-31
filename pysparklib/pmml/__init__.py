e7ea8cb655d20ce4fb776c9f781da69ce7758aa6
deyoulee <deyouwo@gmail.com>
2019-07-31 17:15:05 +0800

    Tuned

--- pysparklib/pmml/__init__.py
+++ pysparklib/pmml/__init__.py
@@ -14,12 +14,11 @@ from pysparklib.pmml.exceptions import PySparkLibError
 
 class PMMLUtil:
 
-    def __init__(self, spark: SparkSession, df: DataFrame, pipeline_dir: str, pmml_dir: str):
+    def __init__(self, spark: SparkSession, df: DataFrame, pipeline_dir: str):
         self.spark = spark
         self.sc: SparkContext = spark.sparkContext
         self.df = df
         self.pipeline_dir = pipeline_dir
-        self.pmml_dir = pmml_dir
         self.stage_dir = f"{self.pipeline_dir}/stages"
         self.fs = self.sc._jvm.org.apache.hadoop.fs.FileSystem.get(self.sc._jsc.hadoopConfiguration())
         self.path = self.sc._jvm.org.apache.hadoop.fs.Path
@@ -41,7 +40,7 @@ class PMMLUtil:
 
     def save_stage(self, stage: Any, stage_order: int):
         stage_path = f"{self.stage_dir}/{stage_order}_{stage.uid}"
-        stage_path_history = f"{self.stage_dir}/{stage_order}_([0-9]|[a-z])*"
+        stage_path_history = f"{self.stage_dir}/{stage_order}_{stage.uid.split('_')[0]}_*"
         status: JavaArray = self.fs.globStatus(self.path(stage_path_history))
         # fileStatus = self.sc._jvm.org.apache.hadoop.fs.FileStatus
         # file_status: fileStatus
@@ -50,25 +49,25 @@ class PMMLUtil:
                 for file_status in status:
                     if self.fs.exists(file_status.getPath()):
                         self.fs.delete(file_status.getPath())
-                        print(f"Deleted stage {file_status.getPath()}")
+                        print(f"Deleted history stage {file_status.getPath()}")
             stage.write().overwrite().save(stage_path)
         except Exception as e:
             raise PySparkLibError(e)
         return stage_path
 
-    def save_pmml(self):
+    def save_pmml(self, pmml_dir: str):
         try:
             metadata_dir = f"{self.pipeline_dir}/metadata"
             metadata_path = self.path(metadata_dir)
             if self.fs.exists(metadata_path):
                 self.fs.delete(metadata_path)
-            if self.fs.exists(self.path(self.pmml_dir)):
-                self.fs.delete(self.path(self.pmml_dir))
+            if self.fs.exists(self.path(pmml_dir)):
+                self.fs.delete(self.path(pmml_dir))
 
             self.sc.parallelize([self._metadata(self.stage_uids)], 1).saveAsTextFile(metadata_dir)
             pipeline_model: PipelineModel = PipelineModel.load(self.pipeline_dir)
             pmml_builder = PMMLBuilder(self.spark, self.df, pipeline_model)
-            self.sc.parallelize([pmml_builder.buildByteArray()], 1).saveAsTextFile(self.pmml_dir)
+            self.sc.parallelize([pmml_builder.buildByteArray()], 1).saveAsTextFile(pmml_dir)
         except Exception as e:
             raise PySparkLibError(e)
-        return self.pmml_dir
+        return pmml_dir
